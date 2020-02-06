04794f90b0b2700682da59890829529dfd0d54b6
Gustavo Brito <britopereiragustavo@gmail.com>
2020-02-06 11:51:49 -0300

    Setting token required to a viewset

--- pontos_turisticos/core/api/viewsets.py
+++ pontos_turisticos/core/api/viewsets.py
@@ -1,5 +1,7 @@
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from .serializers import TouristSpotSerializer
@@ -7,6 +9,8 @@ from pontos_turisticos.core.models import TouristSpot
 
 
 class TouristSpotViewSet(ModelViewSet):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     serializer_class = TouristSpotSerializer
     filterset_fields = ['name']
     filter_backends = (SearchFilter,)
