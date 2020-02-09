29915c778fbca996f9d12c4e57507d10f91f4d22
Gustavo Brito <britopereiragustavo@gmail.com>
2020-02-09 11:12:10 -0300

    adding relationship to the viewsets

--- pontos_turisticos/core/api/serializers.py
+++ pontos_turisticos/core/api/serializers.py
@@ -1,10 +1,22 @@
 from rest_framework.serializers import ModelSerializer
 
 from pontos_turisticos.core.models import TouristSpot
+from pontos_turisticos.address.api.serializers import AddressSerializer
+from pontos_turisticos.comments.api.serializers import CommentSerializer
+from pontos_turisticos.attractions.api.serializers import AttractionSerializer
+from pontos_turisticos.evaluations.api.serializers import EvaluationSerializer
 
 
 class TouristSpotSerializer(ModelSerializer):
+    attractions = AttractionSerializer(many=True)
+    comments = CommentSerializer(many=True)
+    evaluations = EvaluationSerializer(many=True)
+    address = AddressSerializer()
 
     class Meta:
         model = TouristSpot
-        fields = ('id', 'name', 'description', 'approved', 'photo')
+        fields = (
+            'id', 'name', 'description',
+            'approved', 'photo', 'attractions',
+            'comments', 'evaluations', 'address'
+        )
